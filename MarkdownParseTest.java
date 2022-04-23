68e5d27284ed6283a881c10fa0430aafcfdf0cd9
derekcheung11 <100194704+derekcheung11@users.noreply.github.com>
2022-04-22 17:50:29 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,13 +1,26 @@
 import static org.junit.Assert.*; //import junit assert
 import org.junit.*; //import junit
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 public class MarkdownParseTest { //class for test method
     @Test
     public void addition() { //method
         assertEquals(2, 1 + 1); //asserts that two arrays are equal.
     }
 
-    Path filePath = Path.of("test-file.md");
-    String content = Files.readString(filePath);
+    @Test
+    public void getLinks() throws IOException {
+        ArrayList<String> expected = new ArrayList<String>();
+        Path filePath = Path.of("test-file.md");
+        String content = Files.readString(filePath);
+        expected.add("https://something.com");
+        expected.add("some-page.html");
+        ArrayList<String> output = new ArrayList<String>();
+        output = MarkdownParse.getLinks(content);
+        assertEquals(expected.toArray(), output.toArray());
+    }
 
 }
 
